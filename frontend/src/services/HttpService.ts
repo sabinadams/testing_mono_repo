8d24f68468795b81d48323448ff749457d2dd9bd
Sabin Adams <sabintheworld@gmail.com>
2023-01-31 14:48:47 -0800

    Fixes env

--- frontend/src/services/HttpService.ts
+++ frontend/src/services/HttpService.ts
@@ -1,36 +1,36 @@
-import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
-import { toast } from "react-toastify";
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import { toast } from 'react-toastify'
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
   },
-  validateStatus: (status) => status < 500,
-});
+  validateStatus: status => status < 500
+})
 
 const handleRequest = (config: InternalAxiosRequestConfig) => {
-  const user = localStorage.getItem("quoots-user");
+  const user = localStorage.getItem('quoots-user')
   if (user) {
-    const token = JSON.parse(user).token;
+    const token = JSON.parse(user).token
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
   }
-  return config;
-};
+  return config
+}
 
 const handleResponse = (response: AxiosResponse) => {
   if (response.status === 401) {
-    localStorage.removeItem("quoots-user");
+    localStorage.removeItem('quoots-user')
   }
-  return response;
-};
+  return response
+}
 
-instance.interceptors.request.use(handleRequest);
-instance.interceptors.response.use(handleResponse, (e) => {
-  toast.error(e.message);
-});
+instance.interceptors.request.use(handleRequest)
+instance.interceptors.response.use(handleResponse, e => {
+  toast.error(e.message)
+})
 
-export default instance;
+export default instance
